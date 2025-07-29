e8dcdd55abf7acb00f15ec72489ad496e834697d
Abdelrahman Ayman <abdelrahmanayman000@gmail.com>
2025-07-29 04:31:06 +0300

    first assignment

---
+++ assignment_01/README.md
@@ -0,0 +1,1 @@
+my first assignment is here
---
+++ assignment_01/assignment1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# adding and deleting a user
+sudo adduser ayman
+sudo deluser ayman
+
+touch abduu.txt
+
+# changing with two ways
+chmod 654 abduu.txt
+chmod g+x abduu.txt
+
+# make and remove a dir
+mkdir milano
+rm -rf milano
+
+# search with the name
+find . -name "abduu.txt"
+
+# preview data in abduu file
+echo "No.1" >> abduu.txt
+cat test_file.txt
+
+# make a new alias
+alias clr='clear'
+alias m='man'
+m docker
+clr
+
+#nano & history & date
+nano test_file.txt
+history
+date
+
