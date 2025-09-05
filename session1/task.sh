ca44a48dbaf963c8a01dfa89531f31b4fbde9c7b
Abdelrahmangithub <abdelrahmanayman000@gmail.com>
2025-09-05 16:24:19 +0300

    Add submodule contents as normal folders

---
+++ session1/task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# you can run it using sh ./task.sh
+sudo touch abdu.txt
+sudo rm -f abdu.txt
+
+sudo mkdir -p /home/abduu
+sudo rm -rf /home/abduu
+
+sudo mkdir ayman
+sudo chmod 644 ayman
+
+sudo ls -la
